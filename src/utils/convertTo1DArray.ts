0463fd9377407c1ea9e8dde0c2c7070d1496c06e
AZIHubert <allan.aoudji@protonmail.com>
2022-04-09 00:15:19 +0200

    add an exception to convertTo1DArray

--- src/utils/convertTo1DArray.ts
+++ src/utils/convertTo1DArray.ts
@@ -1,4 +1,15 @@
 const convertTo1DArray: <T>(array2D: T[][]) => T[] = (array2D) => {
+  const colsLength = array2D.length;
+
+  if (colsLength !== 3 && colsLength !== 9) {
+    throw new Error('arg should be a 3x3 or a 9x9 matrix');
+  }
+  array2D.forEach((row) => {
+    if (row.length !== colsLength) {
+      throw new Error('arg should be a 3x3 or a 9x9 matrix');
+    }
+  });
+
   const array1D = ([] as typeof array2D[0][0][]).concat(...array2D);
 
   return array1D;
--- test/convertTo1DArray.test.ts
+++ test/convertTo1DArray.test.ts
@@ -1,9 +1,27 @@
 import convertTo1DArray from '@src/utils/convertTo1DArray';
 
+const expectionMessage = 'arg should be a 3x3 or a 9x9 matrix';
+
 describe('convertTo1DArray', () => {
+  it('should throw an error if arg is not a 3x3 array', () => {
+    const wrongArray3x2 = new Array(3).fill(new Array(2));
+    const wrongArray2x3 = new Array(2).fill(new Array(3));
+
+    expect(() => convertTo1DArray(wrongArray3x2)).toThrow(expectionMessage);
+    expect(() => convertTo1DArray(wrongArray2x3)).toThrow(expectionMessage);
+  });
+
+  it('should throw an error if arg is not a 9x9 array', () => {
+    const wrongArray9x8 = new Array(9).fill(new Array(8));
+    const wrongArray8x9 = new Array(8).fill(new Array(9));
+
+    expect(() => convertTo1DArray(wrongArray9x8)).toThrow(expectionMessage);
+    expect(() => convertTo1DArray(wrongArray8x9)).toThrow(expectionMessage);
+  });
+
   it('should convert a 9x9 2D array to a 1D array with a length of 81', () => {
     const shape = 9;
-    const array2D = new Array(shape).fill(new Array(shape).fill(0));
+    const array2D: number[][] = new Array(shape).fill(new Array(shape).fill(0));
     const array1D = convertTo1DArray(array2D);
 
     expect(array1D.length).toBe(shape * shape);
@@ -14,7 +32,7 @@ describe('convertTo1DArray', () => {
 
   it('should convert a 3x3 2D array to a 1D array with a length of 9', () => {
     const shape = 3;
-    const array2D = new Array(shape).fill(new Array(shape).fill(0));
+    const array2D: number[][] = new Array(shape).fill(new Array(shape).fill(0));
     const array1D = convertTo1DArray(array2D);
 
     expect(array1D.length).toBe(shape * shape);
