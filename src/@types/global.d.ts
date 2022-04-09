f11f10144a69b2b03ed1106a9cec82476e506a29
AZIHubert <allan.aoudji@protonmail.com>
2022-04-09 20:14:50 +0200

    add enum Position

--- src/@types/global.d.ts
+++ src/@types/global.d.ts
@@ -6,4 +6,26 @@ declare global {
     Player1 = 'X',
     Player2 = 'Y',
   }
+
+  const enum Position {
+    TopRight,
+    TopMiddle,
+    TopLeft,
+    MiddleRight,
+    MiddleMiddle,
+    MiddleLeft,
+    BottomRight,
+    BottomLeft,
+  }
+
+  const enum WiningLine {
+    TopRow,
+    MiddleRow,
+    BottomRow,
+    LeftColumn,
+    MiddleColumn,
+    RightColumn,
+    TopLeftBottomRightDiagonal,
+    TopRightBottomLeftDiagonal,
+  }
 }
