c272c390cd6b3b9840fa1cf868122be0f082f6ba
AZIHubert <allan.aoudji@protonmail.com>
2022-04-10 00:08:09 +0200

    Create getPosition2DFrom1D.ts

---
+++ src/utils/getPosition2DFrom1D.ts
@@ -0,0 +1,15 @@
+const getPosition2DFrom1D: (
+  position: number,
+  squareSize?: number,
+) => Position2D = (position, squareSize = 9) => {
+  if (position > squareSize * squareSize - 1) {
+    throw new Error('position out of bound');
+  }
+
+  const x = position % squareSize;
+  const y = Math.floor(position / squareSize);
+
+  return {x, y};
+};
+
+export default getPosition2DFrom1D;
