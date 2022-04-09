857e785a40f0cb9cdab1f039d7ed57ceab099328
AZIHubert <allan.aoudji@protonmail.com>
2022-04-09 19:17:45 +0200

    Change X/Y to Player1/Player2 on enum BoardState

--- src/@types/global.d.ts
+++ src/@types/global.d.ts
@@ -3,12 +3,7 @@ export {};
 declare global {
   const enum BoardState {
     Empty = 'EMPTY',
-    X = 'X',
-    Y = 'Y',
-  }
-
-  const enum Player {
-    X = 'X',
-    Y = 'Y',
+    Player1 = 'X',
+    Player2 = 'Y',
   }
 }
