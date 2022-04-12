93a5690c23ad7926aefcbcb8ea2d80c1f9fda809
AZIHubert <allan.aoudji@protonmail.com>
2022-04-12 20:55:37 +0200

    Create getNextMove

---
+++ src/utils/getNextMove.ts
@@ -0,0 +1,65 @@
+import checkIfBoard from './checkIfBoard';
+import checkIfHistory from './checkIfHistory';
+import checkIfMode from './checkIfMode';
+import checkIfSectionIsFull from './checkIfSectionIsFull';
+import checkIfWon from './checkIfWon';
+import convertTo1DArray from './convertTo1DArray';
+import getSubSections from './getSubSections';
+import getTileIndexPositionAndSection from './getTileIndexPositionAndSection';
+import mapMatrix from './mapMatrix';
+
+const getNextMove: (
+  history: number[],
+  board: BoardState[] | Tile[][],
+  mode?: Mode,
+) => number | null = (history, board, mode = Mode.Normal) => {
+  let parseBoard: BoardState[];
+
+  if (!checkIfHistory(history)) {
+    throw new Error('history should be valid');
+  }
+  if (!checkIfBoard(board)) {
+    throw new Error('board should be valid');
+  }
+  if (!checkIfMode(mode)) {
+    throw new Error('mode should be valid');
+  }
+
+  if (board.length === 9) {
+    const stateMatrix = mapMatrix(board as Tile[][], (tile) => tile.state);
+    parseBoard = convertTo1DArray(stateMatrix);
+  } else {
+    parseBoard = [...(board as BoardState[])];
+  }
+
+  if (history.length === 0) {
+    return null;
+  }
+
+  const {position} = getTileIndexPositionAndSection(
+    history[history.length - 1],
+  );
+
+  const sections = getSubSections(parseBoard);
+  const index = sections.findIndex((item) => item.position === position);
+
+  if (index === -1) {
+    throw new Error('position not found');
+  }
+
+  if (
+    mode === Mode.Normal &&
+    checkIfWon(sections[index].tiles) !== BoardState.Empty
+  ) {
+    return null;
+  } else if (
+    mode === Mode.Continue &&
+    checkIfSectionIsFull(sections[index].tiles)
+  ) {
+    return null;
+  }
+
+  return index;
+};
+
+export default getNextMove;
---
+++ tests/getNextMove.test.ts
@@ -0,0 +1,246 @@
+import convertTo2DArray from '@src/utils/convertTo2DArray';
+import generateAssets from '@src/utils/generateAssets';
+import getNextMove from '@src/utils/getNextMove';
+
+const {board} = generateAssets();
+
+describe('getNextMove', () => {
+  it('should throw an error if first arg is not a valid history', () => {
+    const wrongHistories: any[] = [0, 'string', true, {}, [100], new Array(9)];
+
+    wrongHistories.forEach((wrongHistory) => {
+      expect(() => getNextMove(wrongHistory, board)).toThrow(
+        'history should be valid',
+      );
+    });
+  });
+
+  it('should throw an error if second arg is not a valid board', () => {
+    const wrongBoard: any[] = [];
+
+    expect(() => getNextMove([], wrongBoard)).toThrow('board should be valid');
+  });
+
+  it('should throw an error if third arg is not a vlide mode', () => {
+    const wrongModes: any[] = [10, 'string', true, {}, []];
+
+    wrongModes.forEach((wrongMode) => {
+      expect(() => getNextMove([], board, wrongMode)).toThrow(
+        'mode should be valid',
+      );
+    });
+  });
+
+  it('should return null if history.length === 0', () => {
+    const emptyHistory: number[] = [];
+
+    expect(getNextMove(emptyHistory, board)).toBeNull();
+  });
+
+  it('should return 0 if last item of history === 0/3/6/27/30/33/54/57/60', () => {
+    const topLeftTiles = [
+      [1, 0],
+      [50, 18, 3],
+      [6],
+      [10, 11, 12, 13, 27],
+      [30],
+      [33],
+      [10, 11, 54],
+      [57],
+      [60],
+    ];
+
+    topLeftTiles.forEach((tile) => {
+      expect(getNextMove(tile, board)).toBe(0);
+    });
+  });
+
+  it('should return 1 if last item of history === 1/4/7/28/31/34/55/58/61', () => {
+    const topMiddleTiles = [
+      [0, 1],
+      [0, 1, 4],
+      [80, 70, 6, 7],
+      [28],
+      [3, 67, 31],
+      [34],
+      [55],
+      [0, 58],
+      [61],
+    ];
+
+    topMiddleTiles.forEach((tile) => {
+      expect(getNextMove(tile, board)).toBe(1);
+    });
+  });
+
+  it('should return 2 if last item of history === 2/5/8/29/32/35/56/59/62', () => {
+    const topRightTiles = [[2], [5], [8], [29], [32], [35], [56], [59], [62]];
+
+    topRightTiles.forEach((tile) => {
+      expect(getNextMove(tile, board)).toBe(2);
+    });
+  });
+
+  it('should return 3 if last item of history === 9/12/15/36/39/42/63/66/69', () => {
+    const middleLeftTiles = [
+      [9],
+      [12],
+      [15],
+      [36],
+      [39],
+      [42],
+      [63],
+      [66],
+      [69],
+    ];
+
+    middleLeftTiles.forEach((tile) => {
+      expect(getNextMove(tile, board)).toBe(3);
+    });
+  });
+
+  it('should return 4 if last item of history === 10/13/16/37/40/43/64/67/70', () => {
+    const middleMiddleTiles = [
+      [10],
+      [13],
+      [16],
+      [37],
+      [40],
+      [43],
+      [64],
+      [67],
+      [70],
+    ];
+
+    middleMiddleTiles.forEach((tile) => {
+      expect(getNextMove(tile, board)).toBe(4);
+    });
+  });
+
+  it('should return 5 if last item of history === 11/14/17/38/41/44/65/68/71', () => {
+    const middleRightTiles = [
+      [11],
+      [14],
+      [17],
+      [38],
+      [41],
+      [44],
+      [65],
+      [68],
+      [71],
+    ];
+
+    middleRightTiles.forEach((tile) => {
+      expect(getNextMove(tile, board)).toBe(5);
+    });
+  });
+
+  it('should return 6 if last item of history === 18/21/24/45/48/51/72/75/78', () => {
+    const BottomLeftTiles = [
+      [18],
+      [21],
+      [24],
+      [45],
+      [48],
+      [51],
+      [72],
+      [75],
+      [78],
+    ];
+
+    BottomLeftTiles.forEach((tile) => {
+      expect(getNextMove(tile, board)).toBe(6);
+    });
+  });
+
+  it('should return 7 if last item of history === 19/22/25/46/49/52/73/76/79', () => {
+    const BottomMiddleTiles = [
+      [19],
+      [22],
+      [25],
+      [46],
+      [49],
+      [52],
+      [73],
+      [76],
+      [79],
+    ];
+
+    BottomMiddleTiles.forEach((tile) => {
+      expect(getNextMove(tile, board)).toBe(7);
+    });
+  });
+
+  it('should return 8 if last item of history === 20/23/26/47/50/53/74/77/80', () => {
+    const BottomRightTiles = [
+      [20],
+      [23],
+      [26],
+      [47],
+      [50],
+      [53],
+      [74],
+      [77],
+      [80],
+    ];
+
+    BottomRightTiles.forEach((tile) => {
+      expect(getNextMove(tile, board)).toBe(8);
+    });
+  });
+
+  it('should return null if mode === Normal and the next section is already won', () => {
+    const {board: winBoard1} = generateAssets();
+    winBoard1[0] = BoardState.Player1;
+    winBoard1[1] = BoardState.Player1;
+    winBoard1[2] = BoardState.Player1;
+
+    const {board: winBoard2} = generateAssets();
+    winBoard2[0] = BoardState.Player2;
+    winBoard2[10] = BoardState.Player2;
+    winBoard2[20] = BoardState.Player2;
+
+    const {board: winBoard3} = generateAssets();
+    winBoard3[51] = BoardState.Player1;
+    winBoard3[52] = BoardState.Player1;
+    winBoard3[53] = BoardState.Player1;
+
+    expect(getNextMove([0], winBoard1)).toBeNull();
+    expect(getNextMove([0], winBoard2)).toBeNull();
+    expect(getNextMove([17], winBoard3)).toBeNull();
+  });
+
+  it('should return null if mode === CONTINUE and the next section is full', () => {
+    const {board: winBoard1} = generateAssets();
+    winBoard1[0] = BoardState.Player1;
+    winBoard1[1] = BoardState.Player1;
+    winBoard1[2] = BoardState.Player2;
+    winBoard1[9] = BoardState.Player2;
+    winBoard1[10] = BoardState.Player1;
+    winBoard1[11] = BoardState.Player1;
+    winBoard1[18] = BoardState.Player2;
+    winBoard1[19] = BoardState.Player1;
+    winBoard1[20] = BoardState.Player1;
+
+    const {board: winBoard2} = generateAssets();
+    winBoard2[57] = BoardState.Player1;
+    winBoard2[58] = BoardState.Player2;
+    winBoard2[59] = BoardState.Player1;
+    winBoard2[66] = BoardState.Player1;
+    winBoard2[67] = BoardState.Player1;
+    winBoard2[68] = BoardState.Player2;
+    winBoard2[75] = BoardState.Player1;
+    winBoard2[76] = BoardState.Player2;
+    winBoard2[77] = BoardState.Player2;
+
+    expect(getNextMove([6], winBoard1, Mode.Continue)).toBeNull();
+    expect(getNextMove([49], winBoard2, Mode.Continue)).toBeNull();
+  });
+
+  it('should work with a matrix board', () => {
+    const {board} = generateAssets();
+    const matrixBoard = convertTo2DArray(board);
+
+    expect(getNextMove([0], matrixBoard, Mode.Continue)).toBe(0);
+  });
+});
